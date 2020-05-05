2c8feb0e7fde583b67e9b31320bd50b0c854a686
VBLab <58396766+VBLab@users.noreply.github.com>
2020-05-05 17:07:35 +0200

    déplacement ok
    reste la fonction bloccangothere à finir

--- client/js/controller.js
+++ client/js/controller.js
@@ -1,5 +1,23 @@
 function initializeController(grid) {
   window.onkeydown = (event) => {
-    console.log(event.code);
+    // console.log(event.code);
+    let code = event.code;
+    switch (code) {
+      case "ArrowUp": //rotation - sur la flèche du haut//
+        incOrientation(grid);
+        break;
+
+      case "ArrowRight": //déplacement à droite//
+        incX(grid);
+        break;
+
+      case "ArrowDown": //descendre//
+        update(grid);
+        break;
+
+      case "ArrowLeft": //déplacement à gauche//
+        decX(grid);
+        break;
+    }
   };
 }
--- client/js/model.js
+++ client/js/model.js
@@ -8,7 +8,7 @@ function initializeModel(grid) {
   grid.y = -1;
   initializeGrid(grid);
   chooseBloc(grid);
-  window.setInterval(update,250, grid);
+  window.setInterval(update, 250, grid);
 }
 function initializeGrid(grid) {
   for (let i = 0; i < grid.height; i++) {
@@ -29,14 +29,33 @@ function chooseBloc(grid) {
   grid.x = grid.width / 2 - 1;
   grid.y = 0;
 }
+function incOrientation(grid) {
+  grid.orientation = (grid.orientation + 1) % grid.bloc.cells.length;
+}
+function decX(grid) {
+  if (grid.x > 0 && blocCanGoThere(grid,grid.x -1, grid.y)) {
+    grid.x--;
+  }
+}
+function incX(grid) {
+  if (grid.x < grid.width - grid.bloc.cells[grid.orientation][0].length && blocCanGoThere(grid,grid.x+1, grid.y)) {
+    grid.x++;
+  }
+}
+
 function update(grid) {
-  if (grid.y < grid.height - grid.bloc.cells[grid.orientation].length) {
+  if (grid.y < grid.height - grid.bloc.cells[grid.orientation].length && blocCanGoThere(grid,grid.x, grid.y+ 1)) {
     grid.y++;
   } else {
     stockBloc(grid);
     chooseBloc(grid);
   }
 }
+function blocCanGoThere(grid, x, y) {
+  return true;
+// reste à faire pour demain//
+}
+
 function stockBloc(grid) {
   let cells = grid.bloc.cells[grid.orientation];
   for (let i = 0; i < cells.length; i++) {
